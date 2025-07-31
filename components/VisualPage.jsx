36503111da17ad6bc2c4abbe7daa258cc28d6dcf
Richard Hou <xhou@xhou.me>
2025-07-31 15:50:05 +0800

    remove dropzone

--- components/VisualPage.jsx
+++ components/VisualPage.jsx
@@ -1,12 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import InputSection from './Input';
 import OutputSection from './Output';
-import MyDropzone from './Dropzone';
 import VisualHeader from './VisualHeader';
 import "./Visual.css";
 import { generateVisualization } from '../scripts/vis'; // To be fixed later
 
-function VisualPage({ data, loading}) {
+function VisualPage({ data, loading }) {
 
     return (
         <div>
@@ -25,7 +24,7 @@ function VisualPage({ data, loading}) {
                 <div className="flex mt-16 items-center justify-center">
                     <div className='flex flex-col w-full items-center '>
                         <div className='text-3xl font-bold text-orange-800 dark:text-orange-200 mb-8'>Visual your Chimera</div>
-                        <MyDropzone onJsonData={handleData} />
+                        <div>No data provided. Please generate a report with data.</div>
                     </div>
                 </div>
             )}
